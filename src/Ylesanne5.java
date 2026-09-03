5d23f781340c1378ed842ef729fa47c03400d341
rasmus <rasmus.linde@voco.ee>
2026-09-03 10:07:20 +0300

    Ylesanne 1 ja 5

---
+++ src/Ylesanne5.java
@@ -0,0 +1,9 @@
+public class Ylesanne5 {
+    public static void main(String[] args) {
+        // Arvutame sekundite arvu aastas (365 päeva * 24 tundi * 60 minutit * 60 sekundit)
+        int sekundidAastas = 365 * 24 * 60 * 60;
+
+        // Väljastame tulemuse nõutud kujul
+        System.out.println("There are " + sekundidAastas + " seconds in a year.");
+    }
+}
